380e989fab2df788c062f73728fd5c6ea9b561f2
Titiss <Mathis.fcs.lav@gmail.com>
2026-03-23 16:23:49 +0100

    fonctions

--- htdocs/app/Controllers/Employees.php
+++ htdocs/app/Controllers/Employees.php
@@ -1,14 +1,34 @@
 <?php namespace App\Controllers; 
+
 use CodeIgniter\RESTful\ResourceController; 
+
 class Employees extends ResourceController { 
-protected $modelName = 'App\Models\Employees'; 
-protected $format    
-= 'json';  
-public function index(){ 
-return $this->respond($this->model->findAll()); 
-} 
-public function show($id = null) { return $this->respond($this->model->find($id)); }
-public function delete($id = null) { return $this->respond($this->model->delete($id)); }
-public function update($id = null) { return $this->respond($this->model->update($id)); }
-public function create($id = null) { return $this->respond($this->model->insert($id)); }
+    
+    protected $modelName = 'App\Models\Employees'; 
+    protected $format    = 'json';  
+
+    // GET /employees
+    public function index() { 
+        return $this->respond($this->model->findAll()); 
+    } 
+
+    // GET /employees/{id}
+    public function show($id = null) { 
+        return $this->respond($this->model->find($id));
+    }
+
+    // DELETE /employees/{id}
+    public function delete($id = null) { 
+        return $this->respond($this->model->delete($id));
+    }
+
+    // POST /employees
+    public function create() { 
+        return $this->respond($this->model->insert($this->request->getJSON(true)));
+    }
+
+    // PUT /employees/{id}
+    public function update($id = null) {
+        return $this->respond($this->model->update($id, $this->request->getJSON(true)));
+    } 
 }
